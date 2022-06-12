4bd3ce6dbe6f7ca26ff32c70a778c06699ed02fd
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-06-13 00:03:41 +0900

    Use view binding

--- app/src/main/java/com/tfandkusu/androidview/view/main/MainActivity.kt
+++ app/src/main/java/com/tfandkusu/androidview/view/main/MainActivity.kt
@@ -2,31 +2,30 @@ package com.tfandkusu.androidview.view.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
-import com.tfandkusu.androidview.R
+import com.tfandkusu.androidview.databinding.ActivityMainBinding
 import com.tfandkusu.androidview.ui.theme.MyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val adView = findViewById<AdView>(R.id.adView)
         val adRequest = AdRequest.Builder().build()
-        adView.loadAd(adRequest)
-        val composeView = findViewById<ComposeView>(R.id.coomposeView)
-        composeView.setViewCompositionStrategy(
+        binding.adView.loadAd(adRequest)
+        binding.composeView.setViewCompositionStrategy(
             ViewCompositionStrategy.DisposeOnLifecycleDestroyed(
                 this
             )
         )
-        composeView.setContent {
+        binding.composeView.setContent {
             MyTheme {
                 AppContent()
             }
